c8656ac6cf5d3e784339fcabfa2f87540709aff4
yusg <shangang.yu@chiquan-jx.com>
2019-07-24 14:40:56 +0800

    yusg 初始工程创建

---
+++ 10_项目练习/07_源代码/trunk/snail/src/main/java/com/cqkj/snail/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.cqkj.snail.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "欢迎使用SpringBoot";
+    }
+}
